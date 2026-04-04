cc072e5ebcb5ca01314d102607ada17748502205
Vi Phan Tuan <phanquachtuanvi@gmail.com>
2026-04-04 16:22:43 +0700

    update(sg): add ALB, 3rd-party security groups

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -190,31 +190,6 @@ resource "aws_security_group" "public_sg" {
   vpc_id      = aws_vpc.vpc_main.id
   description = "Security group for public subnets which allow HTTP and HTTPS traffic from the internet"
 
-  # ingress = [
-  #   {
-  #     from_port        = 443
-  #     to_port          = 443
-  #     protocol         = "tcp"
-  #     cidr_blocks      = ["0.0.0.0/0"]
-  #     ipv6_cidr_blocks = ["::/0"]
-  #   },
-  #   {
-  #     from_port        = 80
-  #     to_port          = 80
-  #     protocol         = "tcp"
-  #     cidr_blocks      = ["0.0.0.0/0"]
-  #     ipv6_cidr_blocks = ["::/0"]
-  #   }
-  # ]
-
-  # egress {
-  #   from_port        = 0
-  #   to_port          = 0
-  #   protocol         = "-1"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  #   ipv6_cidr_blocks = ["::/0"]
-  # }
-
   lifecycle {
     create_before_destroy = true
   }
@@ -223,23 +198,63 @@ resource "aws_security_group" "public_sg" {
     local.common_tags,
     {
       Name = "${var.project_name}-${var.stage}-sg-public"
+      Tier = "Public"
     }
   )
 }
 
-resource "aws_vpc_security_group_egress_rule" "public_sg_egress" {
-  description       = "Allow all outbound traffic for public subnets"
-  security_group_id = aws_security_group.public_sg.id
+# resource "aws_vpc_security_group_egress_rule" "public_sg_egress" {
+#   description       = "Allow all outbound traffic for public subnets"
+#   security_group_id = aws_security_group.public_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  to_port     = 0
-  ip_protocol = "-1"
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 0
+#   to_port     = 0
+#   ip_protocol = "-1"
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "public_sg_ingress" {
+#   description       = "Allow all inbound traffic for public subnets"
+#   security_group_id = aws_security_group.public_sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 443
+#   to_port     = 443
+#   ip_protocol = "tcp"
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "public_sg_ingress_http" {
+#   description       = "Allow all inbound traffic for public subnets"
+#   security_group_id = aws_security_group.public_sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 80
+#   to_port     = 80
+#   ip_protocol = "tcp"
+# }
+
+# ALB SG
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-${var.stage}-sg-alb"
+  vpc_id      = aws_vpc.vpc_main.id
+  description = "Security group for ALB which allow HTTP and HTTPS traffic from the internet"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.stage}-sg-alb"
+      Tier = "Public"
+    }
+  )
 }
 
-resource "aws_vpc_security_group_ingress_rule" "public_sg_ingress" {
-  description       = "Allow all inbound traffic for public subnets"
-  security_group_id = aws_security_group.public_sg.id
+resource "aws_vpc_security_group_ingress_rule" "alb_sg_ingress_https" {
+  description       = "Allow all inbound traffic for ALB"
+  security_group_id = aws_security_group.alb_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 443
@@ -247,14 +262,25 @@ resource "aws_vpc_security_group_ingress_rule" "public_sg_ingress" {
   ip_protocol = "tcp"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "public_sg_ingress_http" {
-  description       = "Allow all inbound traffic for public subnets"
-  security_group_id = aws_security_group.public_sg.id
+resource "aws_vpc_security_group_ingress_rule" "alb_sg_ingress_http" {
+  description       = "Allow all inbound traffic for ALB"
+  security_group_id = aws_security_group.alb_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 80
   to_port     = 80
   ip_protocol = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_sg_egress" {
+  description       = "Allow all outbound traffic for ALB"
+  security_group_id = aws_security_group.alb_sg.id
+
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.private_sg.id
 }
 
 # Private SG
@@ -271,29 +297,53 @@ resource "aws_security_group" "private_sg" {
     local.common_tags,
     {
       Name = "${var.project_name}-${var.stage}-sg-private"
+      Tier = "Private"
     }
   )
 }
 
-resource "aws_vpc_security_group_egress_rule" "private_sg_egress" {
-  description                  = "Allow all outbound traffic for private subnets"
-  security_group_id            = aws_security_group.private_sg.id
-  referenced_security_group_id = aws_security_group.public_sg.id
-
-  cidr_ipv4   = var.vpc_cidr_block
-  from_port   = 5432
-  to_port     = 5432
-  ip_protocol = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "private_sg_ingress" {
+resource "aws_vpc_security_group_ingress_rule" "private_sg_ingress_alb" {
   description       = "Allow all inbound traffic for private subnets"
   security_group_id = aws_security_group.private_sg.id
 
-  cidr_ipv4   = var.vpc_cidr_block
-  from_port   = 5432
-  to_port     = 5432
-  ip_protocol = "tcp"
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_sg_egress_db" {
+  description       = "Allow all outbound traffic for private subnets"
+  security_group_id = aws_security_group.private_sg.id
+
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.data_sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_sg_egress_cache" {
+  description       = "Allow all outbound traffic for private subnets"
+  security_group_id = aws_security_group.private_sg.id
+
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.data_sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_sg_egress_https" {
+  description       = "Allow HTTPS outbound traffic for private subnets"
+  security_group_id = aws_security_group.private_sg.id
+
+  cidr_ipv4                    = "0.0.0.0/0"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.alb_sg.id
 }
 
 # Data SG
@@ -310,6 +360,48 @@ resource "aws_security_group" "data_sg" {
     local.common_tags,
     {
       Name = "${var.project_name}-${var.stage}-sg-data"
+      Tier = "Data"
+    }
+  )
+}
+
+resource "aws_vpc_security_group_ingress_rule" "data_sg_ingress_db" {
+  description       = "Allow all inbound traffic for data subnets"
+  security_group_id = aws_security_group.data_sg.id
+
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.private_sg.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "data_sg_ingress_cache" {
+  description       = "Allow all outbound traffic for data subnets"
+  security_group_id = aws_security_group.data_sg.id
+
+  cidr_ipv4                    = var.vpc_cidr_block
+  from_port                    = 6379
+  to_port                      = 6379
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.private_sg.id
+}
+
+# Third Party SG
+resource "aws_security_group" "third_party_sg" {
+  name        = "${var.project_name}-${var.stage}-sg-third-party"
+  vpc_id      = aws_vpc.vpc_main.id
+  description = "Security group for third party subnets which allow HTTP and HTTPS traffic from the internet"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.stage}-sg-third-party"
+      Tier = "ThirdParty"
     }
   )
 }
